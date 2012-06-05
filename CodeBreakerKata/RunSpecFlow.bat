9fabe541d1a2c4c3c09fcb14765ba6a7ffc755b1
Juan Jose Fuchs <JuanJo.FuchsC@gmail.com>
2012-06-05 16:26:10 -0500

    Fixed SpecFlow report name

--- CodeBreakerKata/RunSpecFlow.bat
+++ CodeBreakerKata/RunSpecFlow.bat
@@ -1,8 +1,8 @@
-"C:\Program Files (x86)\NUnit\bin\nunit-console-x86.exe" /labels /out=SpecFlowResult.txt /xml=SpecFlowResult.xml /framework=net-4.0 CodeBreaker2.Specs\bin\debug\CodeBreaker2.Specs.dll
+"C:\Program Files (x86)\NUnit\bin\nunit-console-x86.exe" /labels /out=TestResult.txt /xml=TestResult.xml /framework=net-4.0 CodeBreaker2.Specs\bin\debug\CodeBreaker2.Specs.dll
 "C:\Program Files (x86)\TechTalk\SpecFlow\specflow.exe" nunitexecutionreport CodeBreaker2.Specs\CodeBreaker.Specs.csproj"
 
-IF NOT EXIST SpecFlowResult.xml GOTO FAIL
-IF NOT EXIST SpecFlowResult.html GOTO FAIL
+IF NOT EXIST TestResult.xml GOTO FAIL
+IF NOT EXIST TestResult.html GOTO FAIL
 EXIT
 
 :FAIL
